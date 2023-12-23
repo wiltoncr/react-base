5c5db0510965917fb0bcc02c688f418b0687eb8e
Wilton CR <wiltonmeg4@gmail.com>
2023-12-23 17:23:18 -0300

    add colors

---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Title } from './styled';
+import { Container } from '../../styles/GlocalStyles';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={false}>
+        Login
+        <spam>OIe</spam>
+      </Title>
+      <p>Lorem*5</p>
+    </Container>
+  );
+}
