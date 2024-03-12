3cc524bd6629e40fd09bc916dda05314464d321f
Wilton Costa Reis <wiltonmeg4@gmail.com>
2024-03-12 09:42:49 -0300

    revision

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Title } from './styled';
 import { Container } from '../../styles/GlocalStyles';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/aluno');
+      console.log(response);
+    }
+    getData();
+  }, []);
   return (
     <Container>
       <Title />
